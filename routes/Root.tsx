c1e12338c453e6292fd9c1dbd9152e01a217b5e8
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-11-14 13:25:42 +0200

    Create basic structure

---
+++ routes/Root.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { App } from '../components/App';
+import HomePage from '../pages/HomePage';
+import FilmPage from '../pages/FilmPage';
+import NotFoundPage from '../pages/NotFoundPage';
+
+export const Root = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route index element={<HomePage />} />
+      </Route>
+      <Route path='film/:id' element={<FilmPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
